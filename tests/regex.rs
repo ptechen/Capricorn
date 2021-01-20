6f730f51704cc1164c065b2360faa21d083b6fe4
petechen <15736755067@163.com>
2021-01-20 12:47:49 +0800

    update_test

--- tests/regex.rs
+++ tests/regex.rs
@@ -1,7 +1,7 @@
 use capricorn::parse;
 use quicli::prelude::*;
 #[test]
-fn test_each() -> Result<(), Box<dyn std::error::Error>> {
+fn test_regex() -> Result<(), Box<dyn std::error::Error>> {
     let yml = read_file("./test_pages/regex.yml").unwrap();
     let params: parse::HashMapSelectParams = serde_yaml::from_str(&yml).unwrap();
     let html = read_file("./test_pages/regex.html").unwrap();
